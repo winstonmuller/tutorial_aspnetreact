18189d3043b63e1a09b0c2150bce4b074e51221d
Winston Muller <winstonmuller@gmail.com>
2020-09-27 20:47:02 +1300

    Adding Project Files

---
+++ CompleteReactCRUD_Tutorial/Models/DonationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompleteReactCRUD_Tutorial.Models
+{
+    public class DonationDBContext : DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options):base(options)
+        {
+
+        }
+
+        
+    }
+}
